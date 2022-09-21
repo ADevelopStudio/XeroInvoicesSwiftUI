d88cf447edb5acd4594fd53ef0ecac587a568900
Dmitrii Zverev <dima.newmail@gmail.com>
2022-09-21 15:27:43 +1000

    Initial Commit

---
+++ XeroInvoicesSwiftUI/XeroInvoicesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XeroInvoicesSwiftUIApp.swift
+//  XeroInvoicesSwiftUI
+//
+//  Created by Dmitrii Zverev on 21/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct XeroInvoicesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
